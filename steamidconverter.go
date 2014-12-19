1371fe8e975c1f5d1bced956345f2d9a57d8ae7d
babofitos <tehchoi@gmail.com>
2014-12-19 03:58:12 -0500

    add vanity func

--- steamidconverter.go
+++ steamidconverter.go
@@ -5,21 +5,36 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"net/http"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	// "io"
 )
 
-//Steam identifier
-var identifier uint64 = 76561197960265728
+type Steam struct {
+	apikey string
+	client *http.Client
+	identifier uint64
+}
+
+func New(apikey string) *Steam {
+	client := new(http.Client)
+	//Steam identifier
+	var identifier uint64 = 76561197960265728
+	return &Steam{apikey, client, identifier}
+}
 
 //ConvertToText converts a Steam64 ID to STEAM_0:Y:Z format
-func ConvertToText(w uint64) string {
+func (s *Steam) ConvertToText(w uint64) string {
 	y := w % 2
-	v := identifier
+	v := s.identifier
 	z := (w - y - v) / 2
 	return fmt.Sprintf("STEAM_0:%d:%d", y, z)
 }
 
 //ConvertTo64 converts STEAM_X:Y:Z to a Steam64 format
-func ConvertTo64(text string) (uint64, error) {
+func (s *Steam) ConvertTo64(text string) (uint64, error) {
 	sidSlice := strings.Split(text, ":")
 	z, err := strconv.ParseUint(sidSlice[2], 10, 0)
 	if err != nil {
@@ -29,13 +44,13 @@ func ConvertTo64(text string) (uint64, error) {
 	if err != nil {
 		return 0, err
 	}
-	v := identifier
+	v := s.identifier
 	w := z*2 + v + y
 	return w, nil
 }
 
 //ConvertToSteam3 converts STEAM_X:Y:Z to a Steam3 [U:1:W] format
-func ConvertToSteam3(text string) (string, error) {
+func (s *Steam) ConvertToSteam3(text string) (string, error) {
 	sidSlice := strings.Split(text, ":")
 	z, err := strconv.ParseUint(sidSlice[2], 10, 0)
 	if err != nil {
@@ -47,4 +62,47 @@ func ConvertToSteam3(text string) (string, error) {
 	}
 	w := z*2 + y
 	return fmt.Sprintf("[U:1:%d]", w), nil
+}
+
+//ConvertVanityTo64 converts a vanity URL into a Steam64 format
+func (s *Steam) ConvertVanityTo64(vanityUrl string) (uint64, error) {
+	apiReqUrl := s.resolveVUrlHelper(vanityUrl)
+	resp, err := s.client.Get(apiReqUrl)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	type ResponseData struct {
+		Steamid string `json:"steamid"`
+		Success int `json:"success"`
+	}
+	type Response struct {
+		Response ResponseData `json:"response"`
+	}
+	var dat Response
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	err = json.Unmarshal(data, &dat)
+	if err != nil {
+		return 0, err
+	}
+	response := dat
+	success := response.Response.Success
+	if success != 1 {
+		return 0, errors.New("JSON not successful")
+	}
+	sid64 := response.Response.Steamid
+	w, err := strconv.ParseUint(sid64, 10, 0)
+	if err != nil {
+		return 0, err
+	}
+	return w, nil
+}
+
+func (s *Steam) resolveVUrlHelper(url string) string {
+	urlsplit := strings.Split(url, "/")
+	user := urlsplit[len(urlsplit)-2]
+	return fmt.Sprintf("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=%v&vanityurl=%v", s.apikey, user)
 }
--- steamidconverter_test.go
+++ steamidconverter_test.go
@@ -2,6 +2,11 @@ package steamidconverter
 
 import (
 	"testing"
+	"net/http"
+	"net/url"
+	"net/http/httptest"
+	"encoding/json"
+	"strconv"
 )
 
 var textTests = []struct {
@@ -34,9 +39,21 @@ var steam3Tests = []struct {
 	{"STEAM_0:1:2273581", "[U:1:4547163]"},
 }
 
+var vanityTests = []struct {
+	in string
+	out uint64
+}{
+	{"http://steamcommunity.com/id/panvertigo/", 76561198000670105},
+	{"http://steamcommunity.com/id/FireSlash/", 76561197972495328},
+}
+
+var apikey string = ""
+var identifier uint64 = 76561197960265728
+
 func TestConvertToText(t *testing.T) {
+	s := New(apikey)
 	for _, tt := range textTests {
-		sidText := ConvertToText(tt.in)
+		sidText := s.ConvertToText(tt.in)
 		if sidText != tt.out {
 			t.Errorf("ConvertToText(%d) => %q, want %q", tt.in, sidText, tt.out)
 		}
@@ -44,8 +61,9 @@ func TestConvertToText(t *testing.T) {
 }
 
 func TestConvertTo64(t *testing.T) {
+	s := New(apikey)
 	for _, tt := range sixtyFourTests {
-		sid64, err := ConvertTo64(tt.in)
+		sid64, err := s.ConvertTo64(tt.in)
 		if err != nil {
 			t.Errorf("ConvertTo64(%q) => _, %q", tt.in, err)
 		}
@@ -56,13 +74,57 @@ func TestConvertTo64(t *testing.T) {
 }
 
 func TestConvertToSteam3(t *testing.T) {
+	s := New(apikey)
 	for _, tt := range steam3Tests {
-		sid3, err := ConvertToSteam3(tt.in)
+		sid3, err := s.ConvertToSteam3(tt.in)
 		if err != nil {
 			t.Errorf("ConvertToSteam3(%q) => _, %q", tt.in, err)
 		}
 		if sid3 != tt.out {
 			t.Errorf("ConvertToSteam3(%q) => %q, want %q", tt.in, sid3, tt.out)
+		}
+	}
+}
+
+func TestConvertVanityTo64(t *testing.T) {
+	for _, tt := range vanityTests {
+		type ResponseData struct {
+			Steamid string `json:"steamid"`
+			Success int `json:"success"`
+		}
+		type Response struct {
+			Response ResponseData `json:"response"`
+		}
+		var dat Response
+		dat.Response.Steamid = strconv.FormatUint(tt.out, 10)
+		dat.Response.Success = 1
+		b, err := json.Marshal(dat)
+		if err != nil {
+			t.Errorf("ConvertVanity(%q) => _, %q", tt.in, err)
+		}
+		//test server to respond json
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(200)
+    	w.Header().Set("Content-Type", "application/json")
+			w.Write(b)
+		}
+		ts := httptest.NewServer(http.HandlerFunc(handler))
+		defer ts.Close()
+		proxy, err := url.Parse(ts.URL)
+		if err != nil {
+			t.Errorf("ConvertVanity(%q) => _, %q", tt.in, err)
+		}
+		//set client transport to proxy with test server url
+		transport := &http.Transport{Proxy: http.ProxyURL(proxy)}
+		client := &http.Client{Transport: transport}
+		//literal instead of constructor to pass in our custom client
+		s := &Steam{apikey, client, identifier}
+		w, err := s.ConvertVanityTo64(tt.in)
+		if err != nil {
+			t.Errorf("ConvertVanityTo64(%q) => _, %q", tt.in, err)
+		}
+		if w != tt.out {
+			t.Errorf("ConvertVanityTo64(%q) => %d, want %d", tt.in, w, tt.out)
 		}
 	}
 }
