348bac362ffa34cd0290d184963d7666f8d5d998
babofitos <tehchoi@gmail.com>
2014-12-19 00:03:52 -0500

    fix errorf typo

--- steamidconverter_test.go
+++ steamidconverter_test.go
@@ -47,7 +47,7 @@ func TestConvertTo64(t *testing.T) {
 	for _, tt := range sixtyFourTests {
 		sid64, err := ConvertTo64(tt.in)
 		if err != nil {
-			t.Error("ConvertTo64(%q) => _, %q", tt.in, err)
+			t.Errorf("ConvertTo64(%q) => _, %q", tt.in, err)
 		}
 		if sid64 != tt.out {
 			t.Errorf("ConvertTo64(%q) => %d, want %d", tt.in, sid64, tt.out)
@@ -59,7 +59,7 @@ func TestConvertToSteam3(t *testing.T) {
 	for _, tt := range steam3Tests {
 		sid3, err := ConvertToSteam3(tt.in)
 		if err != nil {
-			t.Error("ConvertToSteam3(%q) => _, %q", tt.in, err)
+			t.Errorf("ConvertToSteam3(%q) => _, %q", tt.in, err)
 		}
 		if sid3 != tt.out {
 			t.Errorf("ConvertToSteam3(%q) => %q, want %q", tt.in, sid3, tt.out)
